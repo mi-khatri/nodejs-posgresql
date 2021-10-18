7ec7e7370f9c3eee9470979e21b4e25b8b6e51b3
MiKhatri <55613392+mi-khatri@users.noreply.github.com>
2021-10-18 22:19:28 +0530

    core: minor change

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const env = process.env;
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
     host: env.DB_HOST || 'otto.db.elephantsql.com',
-    port: env.DB_PORT || '5432',
+    port: env.DB_PORT || '3000',
     user: env.DB_USER || 'cklijfef',
     password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
     database: env.DB_NAME || 'cklijfef',
